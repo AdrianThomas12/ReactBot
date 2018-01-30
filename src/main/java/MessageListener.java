9877781a1004e9f22eed2dfcd0a3c4f64daf49f7
Adrian Thomas <akrthomas@gmail.com>
2018-01-30 20:25:42 +1300

    refactoring and added modifiers to roll

--- src/main/java/MessageListener.java
+++ src/main/java/MessageListener.java
@@ -17,27 +17,33 @@ import java.io.IOException;
 import java.util.*;
 import java.util.List;
 
-public class MessageListener extends ListenerAdapter {
+public class MessageListener extends ListenerAdapter
+{
 
     @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        if (event.getAuthor().isBot()) {
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.getAuthor().isBot())
+        {
             return;
         }
         Message message = event.getMessage();
         String content = message.getContent().toUpperCase();
+        MessageChannel channel = event.getChannel();
         Random rand = new Random();
-        if (content.equals("!PING")) {
-            MessageChannel channel = event.getChannel();
+        if (content.equals("!PING"))
+        {
             channel.sendMessage("Pong!").queue();
-        } else if (content.startsWith("!BITCOIN")) {
+        } else if (content.startsWith("!BITCOIN"))
+        {
             String[] currencyList = {"CHF", "HKD", "ISK", "TWD", "EUR", "DKK", "CLP", "USD", "CAD", "INR", "CNY", "THB", "AUD", "KRW", "SGD", "JPY", "PLN", "GBP", "SEK", "NZD", "BRL", "RUB"};
-            MessageChannel channel = event.getChannel();
             CloseableHttpClient httpclient = HttpClients.createDefault();
             HttpGet httpGet = new HttpGet("http://blockchain.info/ticker");
             List<String> currencyArgs = new ArrayList<>(Arrays.asList(content.split(" ")));
-            if (!Collections.disjoint(currencyArgs, Arrays.asList(currencyList)) && currencyArgs.size() >= 2) {
-                try {
+            if (!Collections.disjoint(currencyArgs, Arrays.asList(currencyList)) && currencyArgs.size() >= 2)
+            {
+                try
+                {
                     CloseableHttpResponse response1 = httpclient.execute(httpGet);
                     System.out.println(response1.getStatusLine());
                     HttpEntity entity = response1.getEntity();
@@ -47,14 +53,19 @@ public class MessageListener extends ListenerAdapter {
                     String currencySymbol = bitcoinObject.getString("symbol");
                     String formattedPrice = String.format("%,.2f", currentPrice);
                     channel.sendMessage("Current Price in " + currencyArgs.get(1) + ": " + currencySymbol + formattedPrice).queue();
-                } catch (IOException e) {
+                } catch (IOException e)
+                {
                     e.printStackTrace();
                 }
-            } else {
-                if (Collections.disjoint(currencyArgs, Arrays.asList(currencyList))) {
+            } else
+            {
+                if (Collections.disjoint(currencyArgs, Arrays.asList(currencyList)))
+                {
                     channel.sendMessage("Please use a valid currency \n CHF, HKD, ISK, TWD, EUR, DKK, CLP, USD, CAD, INR, CNY, THB, AUD, KRW, SGD, JPY, PLN, GBP, SEK, NZD, BRL, RUB").queue();
-                } else if (currencyArgs.size() == 1) {
-                    try {
+                } else if (currencyArgs.size() == 1)
+                {
+                    try
+                    {
                         CloseableHttpResponse response1 = httpclient.execute(httpGet);
                         System.out.println(response1.getStatusLine());
                         HttpEntity entity = response1.getEntity();
@@ -63,47 +74,76 @@ public class MessageListener extends ListenerAdapter {
                         float currentPrice = bitcoinObject.getInt("last");
                         String formattedPrice = String.format("%,.2f", currentPrice);
                         channel.sendMessage("Current Price in USD: $" + formattedPrice).queue();
-                    } catch (IOException e) {
+                    } catch (IOException e)
+                    {
                         e.printStackTrace();
                     }
 
                 }
             }
-        } else if (content.startsWith("!EMBEDEDTEST")) {
-            MessageChannel channel = event.getChannel();
-            EmbedBuilder eb = new EmbedBuilder()
-                    .setTitle("This is an embeded Message")
-                    .setColor(event.getMember().getColor())
-                    .setDescription("This is a test description")
-                    .addField("Title of Field", "The contentds of the Field", false)
-                    .setAuthor(event.getAuthor().getName(), null, event.getAuthor().getAvatarUrl())
-                    .setThumbnail("https://raw.githubusercontent.com/rexxars/react-hexagon/HEAD/logo/react-hexagon.png");
-            channel.sendMessage(eb.build()).queue();
-        } else if (content.startsWith("!ROLL")) {
-            MessageChannel channel = event.getChannel();
-            int roll = rand.nextInt(6) + 1; //This results in 1 - 6 (instead of 0 - 5)
-            channel.sendMessage("Your roll: " + roll).queue(sentMessage ->
+        } else if (content.startsWith("!ROLL"))
+        {
+            EmbedBuilder eb;
+            int roll;
+            List<String> modifierArgs = new ArrayList<>(Arrays.asList(content.split(" ")));
+            int diceSize = Integer.valueOf(modifierArgs.get(1));
+            System.out.println(modifierArgs.size());
+            if (modifierArgs.size() == 3)
             {
-                if (roll < 3) {
-                    channel.sendMessage("The roll by user: " + event.getAuthor().getName() + " wasn't very good... Must be bad luck!\n").queue();
+                roll = rand.nextInt(diceSize) + 1; //This results in 1 - 6 (instead of 0 - 5)
+                System.out.println(modifierArgs.get(1));
+                System.out.println(roll);
+                String modifier = modifierArgs.get(2);
+                System.out.println(modifier);
+                String amount = modifier.substring(1);
+                int preModifier = roll;
+                if (modifier.charAt(0) == '+')
+                {
+                    roll = roll + Integer.valueOf(amount);
+                } else
+                {
+                    roll = roll - Integer.valueOf(amount);
                 }
-            });
-        } else if (content.startsWith("!DEFINE")) {
-            MessageChannel channel = event.getChannel();
+//                if (roll >= diceSize)
+//                {
+//                    roll = diceSize;
+//                }
+                if (roll <= 1)
+                {
+                    roll = 1;
+                }
+                eb = new EmbedBuilder()
+                        .setTitle("Roll")
+                        .setColor(new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)))
+                        .setDescription("Original Roll: " + preModifier + "\nModifier: " + modifierArgs.get(2) + "\nFinal Roll: " + roll);
+            } else
+            {
+                roll = rand.nextInt(diceSize) + 1; //This results in 1 - 6 (instead of 0 - 5)
+                eb = new EmbedBuilder()
+                        .setTitle("Roll")
+                        .setColor(new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)))
+                        .setDescription("Roll: " + roll);
+            }
+            channel.sendMessage(eb.build()).queue();
+        } else if (content.startsWith("!DEFINE"))
+        {
             CloseableHttpClient httpclient = HttpClients.createDefault();
             List<String> defineArgs = new ArrayList<>(Arrays.asList(content.split(" ")));
             event.getMessage().delete().queue();
-            if (defineArgs.size() == 1) {
+            if (defineArgs.size() == 1)
+            {
                 channel.sendMessage("Please enter a word to define").queue();
                 return;
             }
             HttpGet httpGet = new HttpGet("http://api.urbandictionary.com/v0/define?term=" + defineArgs.get(1));
-            try {
+            try
+            {
                 CloseableHttpResponse response1 = httpclient.execute(httpGet);
                 System.out.println(response1.getStatusLine());
                 HttpEntity entity = response1.getEntity();
                 JSONObject jsonResponse = new JSONObject(EntityUtils.toString(entity));
-                if (jsonResponse.getString("result_type").equals("exact")) {
+                if (jsonResponse.getString("result_type").equals("exact"))
+                {
                     JSONArray definitionList = jsonResponse.getJSONArray("list");
                     JSONObject listObject = definitionList.getJSONObject(0);
                     EmbedBuilder eb = new EmbedBuilder()
@@ -113,7 +153,8 @@ public class MessageListener extends ListenerAdapter {
 //                            .setThumbnail("https://pbs.twimg.com/profile_images/838627383057920000/m5vutv9g.jpg")
                             .setAuthor("UrbanDictionary", "http://www.urbandictionary.com", "http://is1.mzstatic.com/image/thumb/Purple118/v4/69/92/73/69927356-136b-b772-3bda-fca77248cdd0/source/175x175bb.jpg");
                     channel.sendMessage(eb.build()).queue();
-                } else {
+                } else
+                {
                     EmbedBuilder eb = new EmbedBuilder()
                             .setTitle("Definition of: " + defineArgs.get(1))
                             .setColor(new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)))
@@ -122,9 +163,15 @@ public class MessageListener extends ListenerAdapter {
                             .setAuthor("UrbanDictionary", "http://www.urbandictionary.com", "http://is1.mzstatic.com/image/thumb/Purple118/v4/69/92/73/69927356-136b-b772-3bda-fca77248cdd0/source/175x175bb.jpg");
                     channel.sendMessage(eb.build()).queue();
                 }
-            } catch (IOException e) {
+            } catch (IOException e)
+            {
                 e.printStackTrace();
             }
         }
+    }
+
+    private String getCryptoPrice()
+    {
+        return null;
     }
 }
